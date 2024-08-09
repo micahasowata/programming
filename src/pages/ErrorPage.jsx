34d9d0df581613505776b13972457624ee44b16e
Micah Asowata <andrewgary963@gmail.com>
2024-08-09 20:32:54 +0100

    feat: remove existing files

--- src/pages/ErrorPage.jsx
+++
@@ -1,9 +0,0 @@
-import { useRouteError } from "react-router-dom";
-
-const ErrorPage = () => {
-	const error = useRouteError();
-	console.log(error);
-	return <p>{error.statusText || error.message}</p>;
-};
-
-export default ErrorPage;
