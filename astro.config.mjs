5d1c9893c83eab1f195c257322c713ca577141c9
Micah Asowata <andrewgary963@gmail.com>
2024-08-12 18:11:46 +0100

    feat: add docs for the logout endpoint

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,6 +21,7 @@ export default defineConfig({
           items: [
             { label: "Signup", slug: "users/signup" },
             { label: "Login", slug: "users/login" },
+            { label: "Logout", slug: "users/logout" },
           ],
         },
       ],
