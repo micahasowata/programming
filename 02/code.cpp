cade777a48038555411bb55aded9d5c96f121238
Micah Asowata <andrewgary963@gmail.com>
2025-01-28 12:39:28 +0000

    feat: complete ch2-ex1

--- 02/code.cpp
+++ 02/code.cpp
@@ -1,6 +1,7 @@
-// #include <iostream>
+#include <iostream>
 
 int main() {
-  std::cout << "Hello, World \n";
+  std::cout << "Hello, programming!\n";
+  std::cout << "Here we go!\n";
   return 0;
 }
