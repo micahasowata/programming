3957fee307b8ad148d44123cd2e9449783660f2e
Micah Asowata <andrewgary963@gmail.com>
2025-01-28 13:59:58 +0000

    feat: my attempt at ch2-ex2

--- 02/code.cpp
+++ 02/code.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 
+// goal: direct a computer to the upstairs bathroom
+// assumptions: it understands commands like "climb" and "open the door", it
+// understands direction pointers like "right" and "left" and it is going from
+// the dinning table to the upstairs bathroom
 int main() {
-  std::cout << "Hello, programming!\n";
-  std::cout << "Here we go!\n";
+  std::cout << "walk from the dinning table to the double doors that lead to "
+               "the corridor\n";
+  std::cout << "turn right and walk to the flight of stairs you see\n";
+  std::cout << "climb the stairs till you get upper floor\n";
+  std::cout << "walk towards the door directly in front of you\n";
+  std::cout << "the door directly to the west is the bathroom door\n";
+  std::cout << "walk in and use it\n";
   return 0;
 }
