1d6769957d46170a485d3d40f16aa1fb699c6c8b
Micah Asowata <andrewgary963@gmail.com>
2025-01-29 14:52:34 +0000

    fix: ensure variables are initialized

--- 03/code.cpp
+++ 03/code.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 int main() {
   cout << "what is your first name? (press enter after response)\n";
-  string first_name;
+  string first_name = "";
   cin >> first_name;
 
-  cout << "how old are you? (press enter after response)\n";
-  int age;
+  cout << "how old are you? (press enter after response and can be decimal)\n";
+  double age = 0;
   cin >> age;
 
-  cout << first_name << " is " << age << " years old\n";
+  cout << first_name << " is " << age * 12 << " months old\n";
 }
