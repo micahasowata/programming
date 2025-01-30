5a9ee281643e788b21922f5fb505f797d8f65702
Micah Asowata <andrewgary963@gmail.com>
2025-01-30 12:51:54 +0000

    ch2-ex8

--- 03/code.cpp
+++ 03/code.cpp
@@ -1,38 +1,14 @@
 #include <iostream>
 using namespace std;
+
 int main() {
-  cout << "write two decimal numbers:\n";
-  double val1{0};
-  double val2{0};
-  cin >> val1;
-  cin >> val2;
+  cout << "write a number:\n";
+  int num{0};
+  cin >> num;
 
-  // smaller
-  if (val1 < val2) {
-    cout << val1 << " is smaller than " << val2 << "\n";
+  if (num % 2 == 0) {
+    cout << num << " is even\n";
   } else {
-    cout << val2 << " is smaller than " << val1 << "\n";
+    cout << num << " is odd\n";
   }
-
-  // larger
-  if (val1 > val2) {
-    cout << val1 << " is greater than " << val2 << "\n";
-  } else {
-    cout << val2 << " is greater than " << val1 << "\n";
-  }
-
-  // sum
-  cout << val1 + val2 << " is the sum of " << val1 << " and " << val2 << "\n";
-
-  // difference
-  cout << val1 - val2 << " is the difference of " << val1 << " and " << val2
-       << "\n";
-
-  // product
-  cout << val1 * val2 << " is the product of " << val1 << " and " << val2
-       << "\n";
-
-  // ratio (quotient)
-  cout << val1 / val2 << " is the quotient of " << val1 << " and " << val2
-       << "\n";
 }
