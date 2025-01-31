b382dd6b4301b87998897ac1774dd4b8e768a341
Micah Asowata <andrewgary963@gmail.com>
2025-01-31 23:03:18 +0000

    feat: add more cases to check.
    i cannot suitably handle every edge case that arises as a result of my
    methods maybe, just maybe, when i finally grasp sorting algos. i would
    be able reimagine this project

--- 03/check.cpp
+++ 03/check.cpp
@@ -9,30 +9,40 @@ int main() {
   cin >> y;
   cin >> z;
 
-  // x y
-  if (x > y) {
-    cout << "x is greater than y\n";
-  } else if (x == y) {
-    cout << "x is equal to y\n";
+  if (x > y && y > z && x > z) {
+    cout << z << "\t" << y << "\t" << x << "\n";
+  } else if (x < y && y > z && x > z) {
+    cout << z << "\t" << x << "\t" << y << "\n";
+  } else if (x > y && y < z && x > z) {
+    cout << y << "\t" << z << "\t" << x << "\n";
+  } else if (x > y && y > z && x < z) {
+    cout << x << "\t" << z << "\t" << y << "\n";
   } else {
-    cout << "x is less than y\n";
+    cout << x << "\t" << y << "\t" << z << "\n";
   }
 
-  // y z
-  if (y > z) {
-    cout << "y is greater than z\n";
-  } else if (y == z) {
-    cout << "y is equal to z\n";
-  } else {
-    cout << "y is less than z\n";
-  }
+  //  // x y
+  // if (x == y) {
+  //    cout << "x is equal to y\n";
+  //  } else {
+  //    cout << "x is less than y\n";
+  //  }
 
-  // x z
-  if (x > z) {
-    cout << "x is greater than z\n";
-  } else if (y == z) {
-    cout << "x is equal to z\n";
-  } else {
-    cout << "x is less than z\n";
-  }
+  //  // y z
+  //  if () {
+  //    cout << "y is greater than z\n";
+  //  } else if (y == z) {
+  //    cout << "y is equal to z\n";
+  //  } else {
+  //    cout << "y is less than z\n";
+  //  }
+
+  //  // x z
+  //  if () {
+  //    cout << "x is greater than z\n";
+  //  } else if (y == z) {
+  //    cout << "x is equal to z\n";
+  //  } else {
+  //    cout << "x is less than z\n";
+  //  }
 }
