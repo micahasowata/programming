fef1593a952c1278210b9249f7d293f215d6760a
Micah Asowata <andrewgary963@gmail.com>
2025-01-31 23:44:30 +0000

    feat: re-arrange structure to reflect new book

--- 02/code.cpp
+++ 02/code.cpp
@@ -1,16 +1,25 @@
 #include <iostream>
+using namespace std;
 
-// goal: direct a computer to the upstairs bathroom
-// assumptions: it understands commands like "climb" and "open the door", it
-// understands direction pointers like "right" and "left" and it is going from
-// the dinning table to the upstairs bathroom
 int main() {
-  std::cout << "walk from the dinning table to the double doors that lead to "
-               "the corridor\n";
-  std::cout << "turn right and walk to the flight of stairs you see\n";
-  std::cout << "climb the stairs till you get upper floor\n";
-  std::cout << "walk towards the door directly in front of you\n";
-  std::cout << "the door directly to the west is the bathroom door\n";
-  std::cout << "walk in and use it\n";
-  return 0;
+  cout << "write an operator (+, -, /, *, plus, minus, mul, div)\n";
+  string opr{""};
+  cin >> opr;
+
+  cout << "write two operands:\n";
+  double x{0};
+  double y{0};
+  cin >> x >> y;
+
+  if (opr == "+" || opr == "plus") {
+    cout << x + y << "\n";
+  } else if (opr == "-" || opr == "minus") {
+    cout << x - y << "\n";
+  } else if (opr == "/" || opr == "div") {
+    cout << x / y << "\n";
+  } else if (opr == "*" || opr == "mul") {
+    cout << x * y << "\n";
+  } else {
+    cout << "not a valid operator\n";
+  }
 }
