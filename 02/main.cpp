2c061c37aefbdb7b7b00ed80249cbef214aa33ed
Micah Asowata <andrewgary963@gmail.com>
2025-04-11 21:52:53 +0100

    variable definition

--- 02/main.cpp
+++ 02/main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 
 int main() {
-  std::cout << "Please enter your first name (then press the enter key):\n";
-  std::string first_name;
-  std::cin >> first_name;  // read user input into first_name object
-  std::cout << "Hello, " << first_name << "!\n";
+  std::string name = "annemarie";
+  int num_of_steps = 39;
 }
