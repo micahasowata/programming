a0f47949797faed25b752f1c4acf828071e30adb
Micah Asowata <andrewgary963@gmail.com>
2025-04-11 22:19:55 +0100

    reading mulitiple different val types at once

--- 02/main.cpp
+++ 02/main.cpp
@@ -1,9 +1,14 @@
-#include <string>
+#include <iostream>
 
 int main() {
-  int age = 39;
-  double pi = 3.14;
-  char k = 'k';
-  std::string name = "Annemarie";
-  bool on = true;
+  std::cout << "Please enter your first name and age (then press ENTER):\n";
+
+  // initialize variables with appropriate values
+  std::string first_name = "????";
+  int age = -1;
+
+  // read into first_name before age
+  std::cin >> first_name >> age;
+
+  std::cout << "Hello, " << first_name << " (age " << age << ")\n";
 }
