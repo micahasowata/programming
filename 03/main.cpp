afc8f22465bf8039e270e530272605806c3ea178
Micah Asowata <andrewgary963@gmail.com>
2025-04-16 17:49:23 +0100

    convert if statements to switch

--- 03/main.cpp
+++ 03/main.cpp
@@ -1,31 +1,40 @@
 #include <iostream>
-#include <string>
 
-// converts Japanese Yen, British Pounds and Norwegian Krone into appropriate
-// United States Dollar value exchange rates: wise.com 15th, April 2025
+// converts Japanese Yen, British Pounds, Norwegian Krone and Swiss Francs into
+// appropriate United States Dollar value exchange rates: wise.com 15th, April
+// 2025
 int main() {
   constexpr double yen_to_dollar = 1 / 143.195;
   constexpr double krone_to_dollar = 1 / 10.6464;
   constexpr double pound_to_dollar = 1.3229;
+  constexpr double francs_to_dollar = 1.2278;
 
-  std::string currency = "";
+  char currency = 0;
   double value = 0.0;
 
-  std::cout << "Convert yen (y), pounds (p) or krone (k) into USD.\n";
-  std::cout << "Enter a value followed by a currency (or its symbol):\n";
+  std::cout << "Convert yen(y), pounds (p) or krone (k) into USD.\n";
+  std::cout << "Enter a value followed by a currency symbol:\n";
 
   std::cin >> value >> currency;
 
-  if (currency == "p" || currency == "pounds") {
-    std::cout << value << " " << currency << " = " << value * pound_to_dollar
-              << " dollar(s).\n";
-  } else if (currency == "k" || currency == "krone") {
-    std::cout << value << " " << currency << " = " << value * krone_to_dollar
-              << " dollar(s).\n";
-  } else if (currency == "y" || currency == "yen") {
-    std::cout << value << " " << currency << " = " << value * yen_to_dollar
-              << " dollar(s).\n";
-  } else {
-    std::cout << currency << " is currently not supported.\n";
+  switch (currency) {
+    case 'p':
+      std::cout << value << " pounds = " << value * pound_to_dollar
+                << " dollar(s).\n";
+      break;
+    case 'k':
+      std::cout << value << " krone = " << value * krone_to_dollar
+                << " dollar(s).\n";
+      break;
+    case 'y':
+      std::cout << value << " yen = " << value * yen_to_dollar
+                << " dollar(s).\n";
+      break;
+    case 'f':
+      std::cout << value << " francs = " << value * francs_to_dollar
+                << " dollar(s).\n";
+      break;
+    default:
+      std::cout << currency << " is currently not supported.\n";
   }
 }
