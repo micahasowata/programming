8a3117bcbc55e81e17038075f00e8adff60f47c8
Micah Asowata <andrewgary963@gmail.com>
2025-04-16 17:54:25 +0100

    print all lowercase letters and their ASCII values

--- 03/main.cpp
+++ 03/main.cpp
@@ -1,41 +1,10 @@
 #include <iostream>
 
-// converts Japanese Yen, British Pounds, Norwegian Krone and Swiss Francs into
-// appropriate United States Dollar value exchange rates: wise.com 15th, April
-// 2025
 int main() {
-  constexpr double yen_to_dollar = 1 / 143.195;
-  constexpr double krone_to_dollar = 1 / 10.6464;
-  constexpr double pound_to_dollar = 1.3229;
-  constexpr double francs_to_dollar = 1.2278;
+  int counter = 0;
 
-  char currency = 0;
-  double value = 0.0;
-
-  std::cout
-      << "Convert yen(y), pounds (p), krone (k) or francs (f) into USD.\n";
-  std::cout << "Enter a value followed by a currency symbol:\n";
-
-  std::cin >> value >> currency;
-
-  switch (currency) {
-    case 'p':
-      std::cout << value << " pounds = " << value * pound_to_dollar
-                << " dollar(s).\n";
-      break;
-    case 'k':
-      std::cout << value << " krone = " << value * krone_to_dollar
-                << " dollar(s).\n";
-      break;
-    case 'y':
-      std::cout << value << " yen = " << value * yen_to_dollar
-                << " dollar(s).\n";
-      break;
-    case 'f':
-      std::cout << value << " francs = " << value * francs_to_dollar
-                << " dollar(s).\n";
-      break;
-    default:
-      std::cout << currency << " is currently not supported.\n";
+  while (counter < 26) {
+    std::cout << char('a' + counter) << "\t" << 'a' + counter << '\n';
+    ++counter;
   }
 }
