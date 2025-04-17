94eaffe8bfc89bde37b44800cfdb17fe65eafd7e
Micah Asowata <andrewgary963@gmail.com>
2025-04-17 09:56:57 +0100

    list letter uppercase counterparts

--- 03/main.cpp
+++ 03/main.cpp
@@ -2,6 +2,7 @@
 
 int main() {
   for (int counter = 0; counter < 26; ++counter) {
-    std::cout << char('a' + counter) << "\t" << 'a' + counter << '\n';
+    std::cout << char('a' + counter) << '\t' << 'a' + counter << '\n';
+    std::cout << '\t' << char('A' + counter) << '\t' << 'A' + counter << '\n';
   }
 }
