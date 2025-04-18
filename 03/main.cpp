5854fa187447bd32bad4e737b7c55465f9b68bd1
Micah Asowata <andrewgary963@gmail.com>
2025-04-18 19:59:43 +0100

    drill

--- 03/main.cpp
+++ 03/main.cpp
@@ -1,27 +1,40 @@
 #include <algorithm>
-#include <cmath>
+#include <cstdarg>
 #include <iostream>
 #include <string>
 #include <vector>
 
 int main() {
-  std::vector<std::string> disliked = {"happy", "delighted", "quick", "jiffy"};
-  std::vector<std::string> sentence = {};
+  constexpr double cm_to_m = 1.0 / 100;
+  constexpr double inch_to_m = 1.0 / 39.37;
+  constexpr double foot_to_m = 1.0 / 3.281;
 
-  std::cout << "Write a sentence containing the word happy:\n";
-  std::cout << "input:\n";
-  for (std::string word = ""; std::cin >> word;) {
-    if (std::find(disliked.begin(), disliked.end(), word) != disliked.end()) {
-      sentence.push_back("BLEEP");
+  double value = 0;
+  std::string unit = "";
+  std::vector<double> measurements = {};
+
+  std::cout << "Enter a measurement (units: cm, m, in, ft):\n";
+
+  while (std::cin >> value >> unit) {
+    if (unit == "m") {
+      measurements.push_back(value);
+    } else if (unit == "cm") {
+      measurements.push_back(value * cm_to_m);
+    } else if (unit == "in") {
+      measurements.push_back(value * inch_to_m);
+    } else if (unit == "ft") {
+      measurements.push_back(value * foot_to_m);
     } else {
-      sentence.push_back(word);
+      std::cout << unit << " is currently not supported.\n";
+      return 1;
     }
+  };
+
+  std::sort(measurements.begin(), measurements.end());
+
+  for (double m : measurements) {
+    std::cout << m << "m\t";
   }
 
-  std::cout << "output:\n";
-  for (std::string word : sentence) {
-    std::cout << word << " ";
-  }
-
-  std::cout << '\n';
+  std::cout << "\n";
 }
