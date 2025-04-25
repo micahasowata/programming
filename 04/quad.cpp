1f5cce5b020343bcbec00630db565b1c73c72569
Micah Asowata <andrewgary963@gmail.com>
2025-04-25 15:43:27 +0100

    improve code readability

--- 04/quad.cpp
+++ 04/quad.cpp
@@ -16,29 +16,27 @@ std::vector<double> calc_equation(double a, double b, double c) {
     error("a cannot be 0");
   }
 
-  std::vector<double> roots = {};
+  double discriminant = std::pow(b, 2) - 4 * a * c;
 
-  // find the discriminant to determine presence of imaginary roots
-  double d = std::pow(b, 2) - 4 * a * c;
-
-  if (d < 0) {
+  if (discriminant < 0) {
     error("no real roots");
   }
+
+  std::vector<double> roots = {};
 
   double negative_b = b * -1;
   double twice_a = 2 * a;
 
-  // for +
-  double root_one = (negative_b + std::sqrt(d)) / twice_a;
-  roots.push_back(root_one);
+  double first_root = (negative_b + std::sqrt(discriminant)) / twice_a;
+  roots.push_back(first_root);
 
-  // for -
-  // ensure that repeated roots are handled
-  if (d == 0) {
-    roots.push_back(root_one);
+  // a discriminant == 0 indicates repeated roots
+  // so repeat if necessary else calculate second root
+  if (discriminant == 0) {
+    roots.push_back(first_root);
   } else {
-    double root_two = (negative_b - std::sqrt(d)) / twice_a;
-    roots.push_back(root_two);
+    double second_root = (negative_b - std::sqrt(discriminant)) / twice_a;
+    roots.push_back(second_root);
   }
 
   return roots;
@@ -50,11 +48,12 @@ int main() {
   double c = 0.0;
 
   std::cout << "enter the a, b and c of a quadratic equation:\n";
+
   if (std::cin >> a >> b >> c) {
     try {
       std::vector<double> solutions = calc_equation(a, b, c);
       std::cout << "root one = " << solutions[0]
-                << " root two = " << solutions[1] << '\n';
+                << ", root two = " << solutions[1] << '\n';
     } catch (std::exception& e) {
       std::cerr << e.what();
     }
