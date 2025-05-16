e8b06b7651a37703d620a8c3992445ffe02b48ee
Micah Asowata <andrewgary963@gmail.com>
2025-05-16 14:28:24 +0100

    try this - #1

--- ch_two/main.cpp
+++ ch_two/main.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
 
 int main() {
-  std::cout << "Please enter age and name:\n";
+  std::cout << "Please enter name and age (in years):\n";
+  std::string name = "";
+  double age = 0.0;
 
-  std::string name = "????";
-  int age = -1;
-
-  std::cin >> age;
   std::cin >> name;
+  std::cin >> age;
 
-  std::cout << age << " " << name << "\n";
+  std::cout << "Hello, " << name << " you are " << age * 12 << " months old\n";
 }
