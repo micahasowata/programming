b387f148638d211e98ccbef3fb2dd2475d91b088
Micah Asowata <andrewgary963@gmail.com>
2025-05-16 07:56:07 +0100

    exercise two
    instructions to go to the upstairs bathroom

---
+++ ch_one/ex_two.cpp
@@ -0,0 +1,22 @@
+// should give instructions on how to get to the upstairs bath room from the
+// front door audience: stone aged person
+
+#include <iostream>
+
+int main() {
+  // get to the base of the stairs
+  std::cout << "open the door and walk past it into the corridor then close "
+               "the door behind you\n";
+  std::cout
+      << "keep walking straight up until you come to the starting steps of the "
+         "stairs. it should be on your right with a mirror on your left\n";
+  std::cout << "turn to the right and climb each stair one at a time. support "
+               "yourself with the handrail\n";
+  std::cout << "you should get to a landing tread. pause a little then keep on "
+               "following the progression of the stairs\n";
+  std::cout << "on getting to the top floor keep walking till a mirror is "
+               "directly on your right\n";
+  std::cout << "turn left and walk straight up to the door\n";
+  std::cout << "open it, walk inside and close the door behind you. you are in "
+               "the bathroom\n";
+}
