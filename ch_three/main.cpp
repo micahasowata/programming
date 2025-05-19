bdf7488e24d026180be7a539a370afe814f64796
Micah Asowata <andrewgary963@gmail.com>
2025-05-19 16:18:50 +0100

    currency converter

---
+++ ch_three/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+// converts dollar to japanese yen, norwegian krone and british pounds
+int main() {
+  // rates are the result of 1 dollar / approximate amount of [current currency]
+  // to one dollar
+  constexpr double rate_to_yen = 1.0 / 145.0;
+  constexpr double rate_to_nok = 1.0 / 10.0;
+  constexpr double rate_to_pound = 1.0 / 0.8;
+
+  std::cout << "Convert Norwegian Krone (k), Japanese Yen (y) and British "
+               "Pounds (p) to dollars:\n";
+
+  std::cout
+      << "How much do you want to convert today? (enter amount and symbol):\n";
+  double amount = 0;
+  char unit = ' ';
+  std::cin >> amount >> unit;
+
+  if (amount <= 0) {
+    std::cout << "enter a valid amount\n";
+    return 0;
+  }
+
+  if (unit == 'k') {
+    std::cout << amount << " Krones = " << amount * rate_to_nok
+              << " Dollars.\n";
+  } else if (unit == 'y') {
+    std::cout << amount << " Yen = " << amount * rate_to_yen << " Dollars.\n";
+  } else if (unit == 'p') {
+    std::cout << amount << " Pounds = " << amount * rate_to_pound
+              << " Dollars.\n";
+  } else {
+    std::cout << unit << " is not supported.\n";
+  }
+}
