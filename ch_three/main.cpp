4b9bee3682759eef97966ae345b538878041e90b
Micah Asowata <andrewgary963@gmail.com>
2025-05-19 23:38:39 +0100

    implemented the first sub-routine

--- ch_three/main.cpp
+++ ch_three/main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 
-int main() {
-  constexpr int alphabet_size = 26;
+int square(int x) {
+  int result = 0;
 
-  for (int i = 0; i < alphabet_size; ++i) {
-    std::cout << char('a' + i) << "\t" << int('a' + i) << "\n";
+  for (int i = 0; i < x; ++i) {
+    result += x;
+  }
+
+  return result;
+}
+
+int main() {
+  for (int i = 0; i < 100; ++i) {
+    std::cout << i << "\t" << square(i) << "\n";
   }
 }
