ac3b0eeb244f85cdb2fa9a8b5059557df4e9bd70
Micah Asowata <andrewgary963@gmail.com>
2025-05-19 22:52:53 +0100

    convert from `while` to `for` loop

--- ch_three/main.cpp
+++ ch_three/main.cpp
@@ -3,9 +3,7 @@
 int main() {
   constexpr int alphabet_size = 26;
 
-  int i = 0;
-  while (i < alphabet_size) {
+  for (int i = 0; i < alphabet_size; ++i) {
     std::cout << char('a' + i) << "\t" << int('a' + i) << "\n";
-    ++i;
   }
 }
