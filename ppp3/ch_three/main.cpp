d47fdc29d4613b27d985bce5b5cb3ea9a79f394e
Micah Asowata <andrewgary963@gmail.com>
2025-05-20 08:34:12 +0100

    word redactor

--- ppp3/ch_three/main.cpp
+++ ppp3/ch_three/main.cpp
@@ -1,9 +1,30 @@
+#include <algorithm>
 #include <iostream>
+#include <string>
 #include <vector>
 
 int main() {
-  std::vector<int> v = {5, 7, 9, 4, 6, 8};
-  for (int x : v) {
-    std::cout << x << "\n";
+  std::vector<std::string> disliked = {"broccoli", "rounder", "dimmer"};
+
+  std::cout << "Write a paragraph containing the word Broccoli:\n";
+
+  std::vector<std::string> paragraph = {};
+
+  for (std::string temp = ""; std::cin >> temp;) {
+    paragraph.push_back(temp);
   }
+
+  for (std::size_t i = 0; i < paragraph.size(); ++i) {
+    std::cout << " "; // for in-sentence spacing
+
+    if (std::find(disliked.begin(), disliked.end(), paragraph[i]) !=
+        disliked.end()) {
+      std::cout << "BLEEP";
+      continue;
+    }
+
+    std::cout << paragraph[i];
+  }
+
+  std::cout << "\n"; // new line after redacting
 }
