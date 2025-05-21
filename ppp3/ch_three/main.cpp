4266020577ed6d657ac1c7555b8f4aa45cba9c1d
Micah Asowata <andrewgary963@gmail.com>
2025-05-21 09:50:17 +0100

    find the largest and smallest numbers in a series of input

--- ppp3/ch_three/main.cpp
+++ ppp3/ch_three/main.cpp
@@ -1,35 +1,21 @@
 #include <iostream>
 
-void almost_equal(double x, double y) {
-  constexpr double hundredth = 1.0 / 100.0;
-
-  if (x - y < hundredth) {
-    std::cout << "the numbers are almost equal\n";
-  }
-}
-
 int main() {
-  std::cout << "Enter a pair of integers:\n";
-  double val1 = 0;
-  double val2 = 0;
+  int largest = 0;
+  int smallest = 0;
 
-  while (std::cin >> val1 >> val2) {
-    if (val1 == val2) {
-      std::cout << "the numbers are equal\n";
+  std::cout << "enter a number\n";
+
+  for (int i = 0; std::cin >> i;) {
+    if (i >= largest) {
+      smallest = largest;
+      largest = i;
+      std::cout << "largest so far\n";
     }
 
-    if (val1 > val2) {
-      std::cout << "smaller value: " << val2
-                << ". the larger value is: " << val1 << "\n";
-
-      almost_equal(val1, val2);
-    }
-
-    if (val2 > val1) {
-      std::cout << "the smaller is: " << val1 << ". larger value: " << val2
-                << "\n";
-
-      almost_equal(val2, val1);
+    if (i <= smallest) {
+      smallest = i;
+      std::cout << "smallest so far\n";
     }
   }
 }
