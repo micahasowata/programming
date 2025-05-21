c59c9e47e0c14126a7156871f0d78c8554d5db5b
Micah Asowata <andrewgary963@gmail.com>
2025-05-21 11:57:16 +0100

    sort input with an container

--- ppp3/ch_three/main.cpp
+++ ppp3/ch_three/main.cpp
@@ -1,21 +1,56 @@
+#include <algorithm>
 #include <iostream>
+#include <vector>
 
 int main() {
-  int largest = 0;
-  int smallest = 0;
+  // factors to multiply input unit to get corresponding meters
+  constexpr double cm_to_m = 0.01;
+  constexpr double in_to_m = 0.0254;
+  constexpr double ft_to_m = 0.3048;
 
-  std::cout << "enter a number\n";
+  std::vector<double> dimensions = {};
 
-  for (int i = 0; std::cin >> i;) {
-    if (i >= largest) {
-      smallest = largest;
-      largest = i;
-      std::cout << "largest so far\n";
+  std::cout << "Enter meters (m), centimeters (c), inches (i), feet (f):\n";
+
+  char unit = ' ';
+  double value = 0.0;
+
+  while (std::cin >> value >> unit) {
+    if (value <= 0.0) {
+      std::cout << "\nexiting...\n";
+      break;
     }
 
-    if (i <= smallest) {
-      smallest = i;
-      std::cout << "smallest so far\n";
+    switch (unit) {
+    case 'm':
+      dimensions.push_back(value);
+      break;
+    case 'c':
+      dimensions.push_back(value * cm_to_m);
+      break;
+    case 'i':
+      dimensions.push_back(value * in_to_m);
+      break;
+    case 'f':
+      dimensions.push_back(value * ft_to_m);
+      break;
+    default:
+      std::cout << unit << " is unsupported\n";
     }
+  }
+
+  if (dimensions.size() > 0) {
+    std::ranges::sort(dimensions);
+
+    double sum = 0.0;
+
+    for (double i : dimensions) {
+      sum += i;
+    }
+
+    std::cout << "sum: " << sum << "m\n";
+    std::cout << "total entries: " << dimensions.size() << "\n";
+    std::cout << "largest: " << dimensions.at(dimensions.size() - 1) << "m\n";
+    std::cout << "smallest: " << dimensions.at(0) << "m\n";
   }
 }
