ff23704d5541537e4685368e1e8860d05fe775ed
Micah Asowata <andrewgary963@gmail.com>
2025-05-27 21:42:18 +0100

    generalize function add value to input

--- lgo/funcs/main.go
+++ lgo/funcs/main.go
@@ -8,16 +8,16 @@ func varFunc(i ...int) {
 	}
 }
 
-// returnPlusTwo is a function that returns a function
-func returnPlusTwo() func(x int) int {
+// returnPlus is a function that returns a function
+func returnPlus(toAdd int) func(x int) int {
 	return func(x int) int {
-		return x + 2
+		return x + toAdd
 	}
 }
 
 func main() {
 	varFunc(0, 1, 3, 4, 5, 2, 3, 9, 10, 4)
 
-	p := returnPlusTwo()
-	fmt.Printf("\n2 + 2 = %d\n", p(2))
+	p := returnPlus(5)
+	fmt.Printf("\n5 + 2 = %d\n", p(2))
 }
