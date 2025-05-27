22ab00294e6bfa8969df6d6a9209bdd89f674705
Micah Asowata <andrewgary963@gmail.com>
2025-05-27 20:45:19 +0100

    improve avg
    move the average functionality into a separate function

--- lgo/avg/main.go
+++ lgo/avg/main.go
@@ -1,17 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+// average finds the mean of a slice
+func average(x []float64) float64 {
+	xLen := float64(len(x))
+
+	sum := 0.0
+	for _, i := range x {
+		sum += i
+	}
+
+	return sum / xLen
+}
 
 func main() {
 	s := []float64{0.8452, 0.2134, 0.9821, 0.4567, 0.1239, 0.6543, 0.3219, 0.8765, 0.5678, 0.2198}
+	avg := average(s)
 
-	sum := 0.0
-
-	for _, v := range s {
-		sum += v
-	}
-
-	average := sum / float64(len(s))
-
-	fmt.Printf("average: %.2f\n", average)
+	fmt.Printf("average: %.2f\n", avg)
 }
