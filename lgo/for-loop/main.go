7f45945ab7d42ed56f8a78bb230333c7d5f91c7f
Micah Asowata <andrewgary963@gmail.com>
2025-05-27 20:56:02 +0100

    move doubling logic into a separate function

--- lgo/for-loop/main.go
+++ lgo/for-loop/main.go
@@ -2,11 +2,15 @@ package main
 
 import "fmt"
 
+func double(i int) int {
+	return 2 * i
+}
+
 func main() {
 	var ages [10]int
 
 	for i := range len(ages) {
-		ages[i] = i * 2
+		ages[i] = double(i)
 	}
 
 	fmt.Printf("ages: %v\n", ages)
