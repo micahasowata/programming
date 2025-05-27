577b259ea2b47f2dfcd275729c0054a490420dc1
Micah Asowata <andrewgary963@gmail.com>
2025-05-27 21:47:42 +0100

    create callback function

--- lgo/funcs/main.go
+++ lgo/funcs/main.go
@@ -15,9 +15,27 @@ func returnPlus(toAdd int) func(x int) int {
 	}
 }
 
+// map function
+// applies m on every element in z
+func mMap(m func(int) int, z []int) []int {
+	for k, v := range z {
+		z[k] = m(v)
+	}
+
+	return z
+}
+
 func main() {
 	varFunc(0, 1, 3, 4, 5, 2, 3, 9, 10, 4)
 
 	p := returnPlus(5)
 	fmt.Printf("\n5 + 2 = %d\n", p(2))
+
+	m := func(i int) int {
+		return i * i
+	}
+
+	x := mMap(m, []int{0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144})
+
+	fmt.Printf("%v\n", x)
 }
