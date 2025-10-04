b028b944d46f6b57e52d028fde0d7aef64872f8e
Micah Asowata <micahasowata@gmail.com>
2025-10-04 21:53:20 +0100

    accept multiple input of different types

--- ppp3/obtyva/in_ty.cpp
+++ ppp3/obtyva/in_ty.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <string>
 
+// This program should accept name and age as input and provide a response containing such information
 int main()
 {
     std::cout << "Please enter your first name and age\n";
 
     std::string first_name = "";
-    int age = -1;
+    double age = 0.0;
 
     std::cin >> first_name >> age;
-    std::cout << "Hello, " << first_name << " (age: " << age << ").\n";
+    std::cout << "Hello, " << first_name << " (age: " << age * 12 << " months old).\n";
 }
