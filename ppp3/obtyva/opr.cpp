6f4b39e61d6dd390428b3f3f0db210507ced0a03
Micah Asowata <micahasowata@gmail.com>
2025-10-05 23:55:05 +0100

    a little bit of maths

--- ppp3/obtyva/opr.cpp
+++ ppp3/obtyva/opr.cpp
@@ -1,21 +1,20 @@
 #include <iostream>
-#include <string>
-#include <cmath>
 
 int main()
 {
-    std::cout << "Please enter a integer:\n";
+    // Testing the claim that:
+    // The definitions of integer ∗, /, and % guarantee that for two positive ints a and b we have a/b ∗ b + a%b == a.
+    int a = 7;
+    int b = 2;
 
-    int n = 0;
-    std::cin >> n;
+    std::cout << a / b * b + a % b; // should be the value of a
 
-    std::cout << "n == " << n
-              << "\nn + 1 == " << n + 1
-              << "\nthree times n == " << 3 * n
-              << "\ntwice n == " << n + n
-              << "\nn squared == " << n * n
-              << "\nhalf of n == " << n / 2
-              << "\nsquare root of n == " << sqrt(n)
-              << "\nn modulo 2 == " << n % 2
-              << '\n';
+    // this expression resolves to a + a % b because
+    // a / b * b is a since the b would cancel out each other
+    // for a + a % b == a to be true then a % b has to equal 0
+    // and 7 modulo 2 == 1
+    // unless it resolves as
+    // 7/2 = 3.5 (3 because of integer truncation) multiplied by 2 resulting in 6
+    // then adding that to 7 % 2. That results in 7.
+    // The mathematical and the programming interpretations defer because of the constraints of their environments
 }
