13de396bbb911acc7771cb2f1ca20d1bdbaad773
Micah Asowata <micahasowata@gmail.com>
2025-10-06 11:06:22 +0100

    string operations

--- ppp3/obtyva/opr.cpp
+++ ppp3/obtyva/opr.cpp
@@ -1,20 +1,29 @@
 #include <iostream>
+#include <string>
 
 int main()
 {
-    // Testing the claim that:
-    // The definitions of integer ∗, /, and % guarantee that for two positive ints a and b we have a/b ∗ b + a%b == a.
-    int a = 7;
-    int b = 2;
+    std::cout << "Please enter two names\n";
 
-    std::cout << a / b * b + a % b; // should be the value of a
+    std::string first;
+    std::string second;
 
-    // this expression resolves to a + a % b because
-    // a / b * b is a since the b would cancel out each other
-    // for a + a % b == a to be true then a % b has to equal 0
-    // and 7 modulo 2 == 1
-    // unless it resolves as
-    // 7/2 = 3.5 (3 because of integer truncation) multiplied by 2 resulting in 6
-    // then adding that to 7 % 2. That results in 7.
-    // The mathematical and the programming interpretations defer because of the constraints of their environments
+    std::cin >> first >> second;
+
+    if (first > second)
+    {
+        std::cout << first << " lexicographically comes after " << second << '\n';
+    }
+
+    if (first == second)
+    {
+        std::cout << "that's the same name twice\n";
+    }
+
+    if (first < second)
+    {
+        std::cout << first << " alphabetically comes before " << second << '\n';
+    }
+
+    std::cout << "concat: " << first + second << '\n';
 }
