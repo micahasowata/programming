90b96f69ef513c883460237c11d6175731b9782b
Micah Asowata <micahasowata@gmail.com>
2025-10-10 14:20:49 +0100

    if statements

---
+++ python/if.py
@@ -0,0 +1,9 @@
+x = int(print("enter a number"))
+if x < 0:
+    print("it's negative")
+elif x == 0:
+    print("it is zero")
+elif x > 0:
+    print("it is positive")
+else:
+    print("what did you write") 
