1b35fe0adb4628f5f8322f482a03315994dde00d
Micah Asowata <micahasowata@gmail.com>
2025-12-08 22:49:40 +0000

    read input

---
+++ objects-types-values/read_input.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::cout << "Please enter your first name (followed by \"enter\")\n";
+    std::string first_name;
+    std::cin >> first_name;
+    std::cout << "Hello, " << first_name << "!\n";
+}
